1d836053732932a2da430fea1ca66036e04519c5
ramanabandapu <bandapu.ramana@gmail.com>
2022-06-03 13:09:55 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import { ChakraProvider } from "@chakra-ui/react";
 //TODO: Import and Add ChakraProvider
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
